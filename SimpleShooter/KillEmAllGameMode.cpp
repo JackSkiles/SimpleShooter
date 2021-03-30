a7a3bef652e58aca3a8837cddb707205bf8bf969
Jack Skiles <jackskiles420@gmail.com>
2021-03-30 12:33:47 -0400

    added more endgame funcionality to restart level when player is killed

--- SimpleShooter/KillEmAllGameMode.cpp
+++ SimpleShooter/KillEmAllGameMode.cpp
@@ -7,6 +7,11 @@
 void AKillEmAllGameMode::PawnKilled(APawn* PawnKilled) 
 {
     Super::PawnKilled(PawnKilled);
-    
-    UE_LOG(LogTemp, Warning, TEXT("A pawn was killed!"));
+
+    APlayerController* PlayerController = Cast<APlayerController>(PawnKilled->GetController()); 
+    if(PlayerController != nullptr)
+    {
+        PlayerController->GameHasEnded(nullptr, false);
+    }
+    // UE_LOG(LogTemp, Warning, TEXT("A pawn was killed!"));
 }
